bdc13eda4e40d31bbdf389e815f9c9185224dce1
v0idum <akmal.melibaev@gmail.com>
2018-01-10 21:23:19 +0500

    Contract - init commit

---
+++ src/main/java/com/aven/orderhandler/model/Contract.java
@@ -0,0 +1,29 @@
+package com.aven.orderhandler.model;
+
+public class Contract {
+
+    private final Client[] participants;
+
+    private final Order order;
+
+    private final double totalAmount;
+
+    public Contract(final Client[] participants, final Order order, final double totalAmount) {
+        this.participants = participants;
+        this.order = order;
+        this.totalAmount = order.getItemCost() * order.getItemCount();
+    }
+
+    public Client[] getParticipants() {
+        return participants;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+}
