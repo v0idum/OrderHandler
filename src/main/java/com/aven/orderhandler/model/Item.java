43a7768f8cdc53bfb2ec3b4f830ac3a1f6ffc0cb
v0idum <akmal.melibaev@gmail.com>
2018-02-07 22:17:42 +0500

    cost field/getter have been removed

--- src/main/java/com/aven/orderhandler/model/Item.java
+++ src/main/java/com/aven/orderhandler/model/Item.java
@@ -4,19 +4,12 @@ public class Item {
 
     private final String name;
 
-    private final double cost;
-
-    public Item(final String name, final double cost) {
+    public Item(final String name) {
         this.name = name;
-        this.cost = cost;
     }
 
     public String getName() {
         return name;
-    }
-
-    public double getCost() {
-        return cost;
     }
 
 }
